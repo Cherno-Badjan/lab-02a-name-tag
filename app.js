dfdfc66d6fce6083c2db80e7b903cc1228103210
Cherno Badjan <badjancherno@gmail.com>
2020-12-03 16:51:09 -0800

    three colour buttons

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const myButton = document.getElementById('my-button');
 const myName = document.getElementById('my-name');
 const myInput = document.getElementById('my-input');
-
 myButton.addEventListener('click', () => {
 
-    
     myName.textContent = myInput.value;
     myInput.value = '';
+});
+
+const myBox = document.getElementById('box-color');
+const myGreen = document.getElementById('my-green');
+myGreen.addEventListener('click', () => {
+
+    myBox.style.backgroundColor = 'palegreen';
+
+});
+const myOrange = document.getElementById('my-orange');
+myOrange.addEventListener('click', () => {
+
+    myBox.style.backgroundColor = 'orange';
+
+});
+
+const myPink = document.getElementById('my-pink');
+myPink.addEventListener('click', () => {
+
+    myBox.style.backgroundColor = 'pink';
+
 });
